465508d55ff8b9dc27530a329f3f5e14bc086e65
Lacika <lacimaci2003@gmail.com>
2023-09-26 21:51:32 +0200

    error update

--- Weather_App/app.js
+++ Weather_App/app.js
@@ -6,42 +6,51 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
-    if (city == "")
-    {
-        city = "Kisvárda";
-    }
+
     const response = await fetch(apiUrl + city + `&appid=${apikey}`);
     var data = await response.json();
     console.log(data);
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-    const exp = data.weather[0].main;
-    console.log(exp);
-
-    switch(exp)
+    if (data.cod == "404")
     {
-        case "Clouds":
-            weatherIcon.src = "images/clouds.png";
-            break;
-        case "Clear":
-            weatherIcon.src = "images/clear.png";
-            break;
-        case "Rain":
-            weatherIcon.src = "images/rain.png";
-            break;
-        case "Drizzle":
-            weatherIcon.src = "images/drizzle.png";
-            break;
-        case "Mist":
-            weatherIcon.src = "images/mist.png";
-            break;
-        default:
-            weatherIcon.src = "images/wind.png";
+        document.querySelector(".error").style.display = "block";
     }
+
+    if (data.main.name != "undefined")
+    {
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+        const exp = data.weather[0].main;
+        console.log(exp);
+    
+        switch(exp)
+        {
+            case "Clouds":
+                weatherIcon.src = "images/clouds.png";
+                break;
+            case "Clear":
+                weatherIcon.src = "images/clear.png";
+                break;
+            case "Rain":
+                weatherIcon.src = "images/rain.png";
+                break;
+            case "Drizzle":
+                weatherIcon.src = "images/drizzle.png";
+                break;
+            case "Mist":
+                weatherIcon.src = "images/mist.png";
+                break;
+            default:
+                weatherIcon.src = "images/wind.png";
+        }
+    
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+
 }
 
 searchBtn.addEventListener("click", () => {
@@ -54,5 +63,4 @@ document.addEventListener("keypress", function(event){
     }
 });
 
-const city = "Kisvárda";
-checkWeather(city);
+checkWeather();
